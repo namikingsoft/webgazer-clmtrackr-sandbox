98dbc3c98163bfe32a7b3d34a68a4b753e107157
Tsubasa Namiki <namikingsoft@gmail.com>
2016-10-15 22:50:42 +0900

    chore: add eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,35 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import",
+    "flowtype",
+  ],
+  "rules": {
+    "no-console": 0,
+    "no-underscore-dangle": 0,
+    "import/prefer-default-export": 0,
+    "import/no-extraneous-dependencies": 0,
+    "flowtype/define-flow-type": 1,
+    "flowtype/use-flow-type": 1,
+    "flowtype/valid-syntax": 1,
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+    "mocha": true,
+    "node": true,
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "moduleDirectory": [
+          "node_modules",
+          "src",
+        ],
+      },
+    },
+  },
 };
