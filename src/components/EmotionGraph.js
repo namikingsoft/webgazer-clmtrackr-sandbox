7140fd0d7f2b8a0aff9e6bb7e5436c8e51473183
Tsubasa Namiki <namikingsoft@gmail.com>
2016-10-18 09:52:04 +0900

    WIP

--- src/components/EmotionGraph.js
+++ src/components/EmotionGraph.js
@@ -11,7 +11,15 @@ type Props = {
 
 const EmotionGraph = ({ className, predictions }: Props) => (
   <div className={classnames(className, style.my)}>
-    {predictions.map(x => <div key={x.name}>{x.name}: {x.value}</div>)}
+    {predictions.map(x =>
+      <div
+        key={x.name}
+        className={style.myGraph}
+        style={{
+          width: `${Math.round(x.value * 100)}%`,
+        }}
+      >{x.name}</div>
+    )}
   </div>
 );
 
